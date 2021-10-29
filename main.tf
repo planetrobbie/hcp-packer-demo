ff2b3f71f24c78ed65cdabd2f600cb333a941b9a
Sébastien Braun <sebbraun@gmail.com>
2021-10-29 11:33:58 +0200

    terraform packer integration demo

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "~> 0.17.0"
+    }
+  }
+}
+
+data "hcp_packer_iteration" "vault" {
+	bucket_name = var.bucket
+	channel = var.channel
+}
+
+data "hcp_packer_image" "vault-image" {
+	bucket_name = var.bucket
+	iteration_id = data.hcp_packer_iteration.vault.ulid
+	cloud_provider = "gce"
+	region = var.region
+}
+
+output "vault-production-image-id" {
+	value = data.hcp_packer_image.vault-image.cloud_image_id
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "region where to operate"
+  default = "europe-west1-c"
+}
+
+variable "bucket" {
+  description = "HCP Packer Bucket"
+}
+
+variable "channel" {
+  description = "HCP Packer Channel"
+}
