058463618a1a3aed2d25d2171dc386635fb6168a
Sébastien Braun <sebbraun@gmail.com>
2021-10-29 14:38:05 +0200

    vault 1.8.4

--- files/script.sh
+++ files/script.sh
@@ -2,6 +2,6 @@
 sudo su <<HERE
 apt-get update
 apt-get install unzip
-curl https://releases.hashicorp.com/vault/1.8.3/vault_1.8.3_linux_amd64.zip --output /usr/local/bin/vault.zip
+curl https://releases.hashicorp.com/vault/1.8.4/vault_1.8.4_linux_amd64.zip --output /usr/local/bin/vault.zip
 unzip /usr/local/bin/vault.zip -d /usr/local/bin/
 HERE
