8f4f4993deb70309e7006fb327be1c2ab2af8c44
Sebastien Braun <sebastien@hashicorp.com>
2023-01-06 11:08:05 +0000

    Vault 1.12.1

--- files/script.sh
+++ files/script.sh
@@ -2,6 +2,6 @@
 sudo su <<HERE
 apt-get update
 apt-get install unzip
-curl https://releases.hashicorp.com/vault/1.12.0/vault_1.12.0_linux_amd64.zip --output /usr/local/bin/vault.zip
+curl https://releases.hashicorp.com/vault/1.12.1/vault_1.12.1_linux_amd64.zip --output /usr/local/bin/vault.zip
 unzip /usr/local/bin/vault.zip -d /usr/local/bin/
 HERE
